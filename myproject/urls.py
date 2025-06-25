480cc3fd5dcdbf00d1d86f4c047781ff797ca277
Suvradip01 <ghoshsuvradip215@gmail.com>
2025-06-25 03:08:01 -0400

    Initial commit for online course Django project

--- myproject/urls.py
+++ myproject/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-from django.conf.urls.static import static
-from django.conf import settings
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('onlinecourse/', include('onlinecourse.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', RedirectView.as_view(url='/onlinecourse/', permanent=True)),
+]
--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -2,7 +2,24 @@ from django.contrib import admin
 # <HINT> Import any new Models here
 from .models import Course, Lesson, Instructor, Learner
 
+from .models import Enrollment  # Add this import
+
+admin.site.register(Enrollment)  # Add this line at the bottom
+
+
 # <HINT> Register QuestionInline and ChoiceInline classes here
+from .models import Question, Choice, Submission
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
 
 
 class LessonInline(admin.StackedInline):
